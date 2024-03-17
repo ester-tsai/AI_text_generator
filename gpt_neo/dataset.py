0b0a498ad9180f732c1f919af3552b48a67dff96
sjchu <samuelchu90@gmail.com>
2024-03-17 05:40:35 +0000

    still working

--- gpt_neo/dataset.py
+++ gpt_neo/dataset.py
@@ -4,7 +4,7 @@ from transformers import GPT2Tokenizer
 from torch.utils.data import Dataset
 
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
-MAX_TOKEN_LENGTH = 256
+MAX_TOKEN_LENGTH = 100 
 
 class TextDataset(Dataset):
     def __init__(self, dataset_path):
@@ -22,7 +22,7 @@ class TextDataset(Dataset):
             
             for line in self.data:
                 encodings = tokenizer(line, truncation=True, max_length=MAX_TOKEN_LENGTH, padding="max_length")
-
+                
                 self.input_ids.append(torch.tensor(encodings['input_ids']))
                 self.attention_mask.append(torch.tensor(encodings['attention_mask'])) # attention mask to ignore padding
 
--- gpt_neo/main.py
+++ gpt_neo/main.py
@@ -1,8 +1,19 @@
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 from dataset import TextDataset
+from torch.utils.data import DataLoader
+import torch
+from tqdm import tqdm
 
-#model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
-#tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device = "cpu"
+model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
+model = model.to(device)
+tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
+
+#inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+#outputs = model(**inputs, labels=inputs["input_ids"])
+#print(outputs.logits)
 
 #prompt = (
 #    "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
@@ -20,4 +31,49 @@ from dataset import TextDataset
 #)
 #gen_text = tokenizer.batch_decode(gen_tokens)[0]
 
+batch_size = 4 
+epochs = 5
+learning_rate = .01
+epsilon = 1e-8
+gamma = 0.95
+max_token_length = 100
+
+#create dataloader
 dataset = TextDataset('../data/cleaned_with_name.txt')
+dataloader = DataLoader(dataset, batch_size, shuffle=True)
+
+#set up optimizer
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, eps=epsilon)
+
+#set up scheduler
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma)
+
+#resize token embeddings
+model.resize_token_embeddings(len(tokenizer))
+
+#train model
+model.train()
+for epoch in range(epochs):
+    for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+        input_ids = data[0].to(device)
+        masks = data[1].to(device)
+        labels = data[0].to(device)
+        print(input_ids.shape)
+        print(masks.shape)
+        print(labels.shape)
+
+        model.zero_grad()
+
+        print(input_ids.device)
+        print(masks.device)
+        #outputs = model(input_ids=input_ids, attention_mask=masks, labels = labels)
+        outputs = model(input_ids, attention_mask=masks, labels=None)
+        loss = outputs[0]
+        total_train_loss += loss.item()
+
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+    print(f"epoch: {epoch}, loss = {total_train_loss/len(dataloader)}")
+
