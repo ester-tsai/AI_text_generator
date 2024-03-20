78fff4f0e4a349bbaaa5eaf76f6fca1a6918823c
sjchu <samuelchu90@gmail.com>
2024-03-20 00:24:57 +0000

    still trying

--- gpt_neo/main.py
+++ gpt_neo/main.py
@@ -11,7 +11,8 @@ model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
 model = model.to(device)
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
 
-#inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+print(f'inputs{inputs}')
 #outputs = model(**inputs, labels=inputs["input_ids"])
 #print(outputs.logits)
 
@@ -51,23 +52,47 @@ scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma)
 #resize token embeddings
 model.resize_token_embeddings(len(tokenizer))
 
+
+input_ids_test = [   27,    91,  9688,  1659,  5239,    91,    29, 17100,    25, 19462,
+           262, 13439,   836,   447,   247,    83,   787,  2565, 30325,   255,
+         50256, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
+         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257]
+
+attention_mask_test = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0] 
+
+inputs_test = {'input_ids': torch.tensor(input_ids_test), 'attention_mask': torch.tensor(attention_mask_test)}
+#inputs = {'input_ids': tensor([[15496,    11,   616,  3290,   318, 13779]]), 'attention_mask': tensor([[1, 1, 1, 1, 1, 1]])}
+
 #train model
 model.train()
 for epoch in range(epochs):
     for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
         input_ids = data[0].to(device)
-        masks = data[1].to(device)
+        attention_mask = data[1].to(device)
         labels = data[0].to(device)
-        print(input_ids.shape)
-        print(masks.shape)
-        print(labels.shape)
+        print(f'input_ids.shape{input_ids.shape}')
+        print(f'attention_mask.shape{attention_mask.shape}')
+        print(f'labels.shape{labels.shape}')
+        print(f'input_ids{input_ids}')
+        print(f'attention_mask{attention_mask}')
 
         model.zero_grad()
 
         print(input_ids.device)
-        print(masks.device)
-        #outputs = model(input_ids=input_ids, attention_mask=masks, labels = labels)
-        outputs = model(input_ids, attention_mask=masks, labels=None)
+        print(attention_mask.device)
+        outputs = model(**inputs_test, labels=input_ids)
+        #outputs = model(input_ids=input_ids_test, attention_mask=attention_mask_test)
+        #outputs = model(input_ids, attention_mask=masks, labels=None)
         loss = outputs[0]
         total_train_loss += loss.item()
 
