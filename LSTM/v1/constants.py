4b5d12d47290d556d872d37c8fd774656dd79cff
ester-tsai <tsaiester@gmail.com>
2024-03-21 15:33:10 -0700

    baseline uses chat data without names

--- LSTM/v1/constants.py
+++ LSTM/v1/constants.py
@@ -1,2 +1,2 @@
-INPUT_TRAIN_PATH = '../../data/cleaned_with_name.txt'  # File path for the training data
-INPUT_VAL_PATH = '../../data/cleaned_with_name.txt'  # File path for the validation data
+INPUT_TRAIN_PATH = '../../data/cleaned.txt'  # File path for the training data
+INPUT_VAL_PATH = '../../data/cleaned.txt'  # File path for the validation data
