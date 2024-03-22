a89531e3407bccbe37bd378e80b287983129640a
Samuel Chu <samuelchu90@gmail.com>
2024-03-21 19:39:10 -0700

    main problem: using padding with the tokenizer, breaks the code. specifically when data is passed through the model.

--- gpt_neo/dataset.py
+++ gpt_neo/dataset.py
@@ -4,7 +4,7 @@ from transformers import GPT2Tokenizer
 from torch.utils.data import Dataset
 
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
-MAX_TOKEN_LENGTH = 100 
+MAX_TOKEN_LENGTH = 25
 
 class TextDataset(Dataset):
     def __init__(self, dataset_path):
@@ -22,6 +22,7 @@ class TextDataset(Dataset):
             
             for line in self.data:
                 encodings = tokenizer(line, truncation=True, max_length=MAX_TOKEN_LENGTH, padding="max_length")
+                #encodings = tokenizer(line, return_tensors="pt")
                 
                 self.input_ids.append(torch.tensor(encodings['input_ids']))
                 self.attention_mask.append(torch.tensor(encodings['attention_mask'])) # attention mask to ignore padding
--- gpt_neo/main.py
+++ gpt_neo/main.py
@@ -4,6 +4,13 @@ from torch.utils.data import DataLoader
 import torch
 from tqdm import tqdm
 
+batch_size = 4 
+epochs = 5
+learning_rate = .01
+epsilon = 1e-8
+gamma = 0.95
+max_token_length = 25
+
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 device = "cpu"
@@ -11,33 +18,25 @@ model = GPTNeoForCausalLM.from_pretrained("EleutherAI/gpt-neo-1.3B")
 model = model.to(device)
 tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-1.3B")
 
-inputs = tokenizer("Hello, my dog is cute", return_tensors="pt")
+
+tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+inputs_broken = tokenizer("Hello, my dog is cute. Make this sentence longer and longer to test the functionality" \
+    , truncation=True, max_length=max_token_length, padding="max_length", return_tensors="pt")
+
+
+inputs = tokenizer("Hello, my dog is cute. Make this sentence longer and longer to test the functionality", return_tensors="pt")
+
+print(f'inputs_broken{inputs_broken}')
+print(f'inputs_broken_input_ids{inputs_broken["input_ids"].size()}')
+print(f'inputs_broken_att_mask{inputs_broken["attention_mask"].size()}')
 print(f'inputs{inputs}')
-#outputs = model(**inputs, labels=inputs["input_ids"])
-#print(outputs.logits)
 
-#prompt = (
-#    "In a shocking finding, scientists discovered a herd of unicorns living in a remote, "
-#    "previously unexplored valley, in the Andes Mountains. Even more surprising to the "
-#    "researchers was the fact that the unicorns spoke perfect English."
-#)
+outputs = model(**inputs_broken, labels=inputs["input_ids"])
+print(f'outputs1{outputs.logits}')
 
-#input_ids = tokenizer(prompt, return_tensors="pt").input_ids
 
-#gen_tokens = model.generate(
-#    input_ids,
-#    do_sample=True,
-#    temperature=0.9,
-#    max_length=100,
-#)
-#gen_text = tokenizer.batch_decode(gen_tokens)[0]
-
-batch_size = 4 
-epochs = 5
-learning_rate = .01
-epsilon = 1e-8
-gamma = 0.95
-max_token_length = 100
+outputs = model(**inputs, labels=inputs["input_ids"])
+print(outputs.logits)
 
 #create dataloader
 dataset = TextDataset('../data/cleaned_with_name.txt')
@@ -53,30 +52,13 @@ scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=gamma)
 model.resize_token_embeddings(len(tokenizer))
 
 
-input_ids_test = [   27,    91,  9688,  1659,  5239,    91,    29, 17100,    25, 19462,
-           262, 13439,   836,   447,   247,    83,   787,  2565, 30325,   255,
-         50256, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257,
-         50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257, 50257]
-
-attention_mask_test = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
-         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-         0, 0, 0, 0] 
-
-inputs_test = {'input_ids': torch.tensor(input_ids_test), 'attention_mask': torch.tensor(attention_mask_test)}
-#inputs = {'input_ids': tensor([[15496,    11,   616,  3290,   318, 13779]]), 'attention_mask': tensor([[1, 1, 1, 1, 1, 1]])}
 
 #train model
+total_train_loss = 0
 model.train()
 for epoch in range(epochs):
     for i, data in tqdm(enumerate(dataloader), total=len(dataloader)):
+        print(f'data{data}')
         input_ids = data[0].to(device)
         attention_mask = data[1].to(device)
         labels = data[0].to(device)
@@ -90,7 +72,7 @@ for epoch in range(epochs):
 
         print(input_ids.device)
         print(attention_mask.device)
-        outputs = model(**inputs_test, labels=input_ids)
+        outputs = model(input_ids = input_ids, attention_mask = attention_mask)
         #outputs = model(input_ids=input_ids_test, attention_mask=attention_mask_test)
         #outputs = model(input_ids, attention_mask=masks, labels=None)
         loss = outputs[0]
